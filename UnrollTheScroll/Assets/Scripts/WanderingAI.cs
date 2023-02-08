519c24b9f59730abc12aa1a664b527921f4201bc
Jared Lee <jared.lee@intellitect.com>
2023-02-08 09:18:00 -0800

    Adds empty callback to WanderingAI script so exception is not thrown.

--- UnrollTheScroll/Assets/Scripts/WanderingAI.cs
+++ UnrollTheScroll/Assets/Scripts/WanderingAI.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class WanderingAI : MonoBehaviour
@@ -12,17 +11,11 @@ public class WanderingAI : MonoBehaviour
 
     // animation IDs
     private int _animIDSpeed;
-    private int _animIDGrounded;
-    private int _animIDJump;
-    private int _animIDFreeFall;
     private int _animIDMotionSpeed;
 
     private void AssignAnimationIDs()
     {
         _animIDSpeed = Animator.StringToHash("Speed");
-        _animIDGrounded = Animator.StringToHash("Grounded");
-        _animIDJump = Animator.StringToHash("Jump");
-        _animIDFreeFall = Animator.StringToHash("FreeFall");
         _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
     }
 
@@ -58,6 +51,11 @@ public class WanderingAI : MonoBehaviour
                 transform.Rotate(0, angle, 0);
             }
         }
+    }
 
+    void OnFootstep()
+    {
+        // This function is called by the animator class, and if it is missing
+        // an exception is thrown.
     }
 }
