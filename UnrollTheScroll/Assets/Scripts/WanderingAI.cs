b947bb0f969e027d7d2b3a4992bb707b5b20b3cf
Jared Lee <jared.lee@intellitect.com>
2023-02-07 23:06:48 -0800

    Adds animation ability to WanderingAI.

--- UnrollTheScroll/Assets/Scripts/WanderingAI.cs
+++ UnrollTheScroll/Assets/Scripts/WanderingAI.cs
@@ -7,15 +7,45 @@ public class WanderingAI : MonoBehaviour
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     
+    private Animator _animator;
+    private bool _hasAnimator;
+
+    // animation IDs
+    private int _animIDSpeed;
+    private int _animIDGrounded;
+    private int _animIDJump;
+    private int _animIDFreeFall;
+    private int _animIDMotionSpeed;
+
+    private void AssignAnimationIDs()
+    {
+        _animIDSpeed = Animator.StringToHash("Speed");
+        _animIDGrounded = Animator.StringToHash("Grounded");
+        _animIDJump = Animator.StringToHash("Jump");
+        _animIDFreeFall = Animator.StringToHash("FreeFall");
+        _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the Animator attached to the GameObject.
+        _hasAnimator = TryGetComponent(out _animator);
+        AssignAnimationIDs();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        // Trigger the Animator's Walk animation.
+        if (_hasAnimator)
+        {
+            _animator.SetFloat(_animIDSpeed, 2);
+            _animator.SetFloat(_animIDMotionSpeed, 1);
+        }
+
+        
         transform.Translate(0, 0, speed * Time.deltaTime);
 
         Ray ray = new Ray(transform.position, transform.forward);
